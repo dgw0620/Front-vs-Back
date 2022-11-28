3c01dbb93de7d154be1e2132459d35a251292996
dgw0620 <dgw0620@naver.com>
2022-11-28 19:23:00 +0900

    로딩 화면 생성

---
+++ app/src/main/java/com/android/front_vs_back/LodingActivity.java
@@ -0,0 +1,24 @@
+package com.android.front_vs_back;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class LodingActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+        startLoading();
+    }
+
+    private void startLoading() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+            }
+        }, 2000);
+    }
+}
