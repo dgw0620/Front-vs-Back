80695b0cad4c118abeffeac234d09ce10119adf3
dgw0620 <dgw0620@naver.com>
2022-12-02 03:06:25 +0900

    결과 버튼 생성

--- app/src/main/java/com/android/front_vs_back/MainActivity.java
+++ app/src/main/java/com/android/front_vs_back/MainActivity.java
@@ -1,10 +1,13 @@
 package com.android.front_vs_back;
 
-import androidx.appcompat.app.ActionBar;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -16,9 +19,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.hide();
 
         Button startBtn = (Button) findViewById(R.id.startBtn);
 
@@ -44,4 +44,5 @@ public class MainActivity extends AppCompatActivity {
             finishAffinity();
         }
     }
+
 }
